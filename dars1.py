d712113b8bbf2bde066c6e7c22d2f9eada4729d8
Ramziddin07 <bekjonovramziddin5@gmail.com>
2024-06-10 14:35:14 +0500

    Create dars1.py
    dars1

---
+++ dars1.py
@@ -0,0 +1,22 @@
+a =5*4
+print(a)
+
+b= 22%4
+print(b)
+
+d=12
+p=3.14
+r=d/2
+print(p*(r**2))
+
+
+print(81**(1/2))
+print(27**(1/3))
+
+print('Ganisher dukondan "Python asoslari"\n degan kitob oldm!\nBu kitobda python urganish mumkin.')
+
+a=8
+print(f"8 ning 2 darjasi{a**2}")
+
+d=27%4
+print(d)
