0df264bf9d045d01fe1836238fe05eaa8c572b54
Ramziddin07 <bekjonovramziddin5@gmail.com>
2024-10-23 16:24:33 +0500

    Update vazifa10.py

--- vazifa10.py
+++ vazifa10.py
@@ -1,55 +1,12 @@
-class Shaxs:
-    def __init__(self, ismi, familiyasi):
-        self.ismi = ismi
-        self.familiyasi = familiyasi
-
-    def get_info(self):
-        return f"{self.ismi} {self.familiyasi}"
-
-class Professor(Shaxs):
-    def __init__(self, ismi, familiyasi, fakultet):
-        super().__init__(ismi, familiyasi)
-        self.fakultet = fakultet
-
-    def get_info(self):
-        return f"Professor: {super().get_info()}, Fakultet: {self.fakultet}"
-
-class Foydalanuvchi(Shaxs):
-    def __init__(self, ismi, familiyasi, email):
-        super().__init__(ismi, familiyasi)
+class User:
+    def __init__(self, ism, familiya, email,adress):
+        self.ism = ism
+        self.familiya = familiya
         self.email = email
+        self.adress = adress
 
     def get_info(self):
-        return f"Foydalanuvchi: {super().get_info()}, Email: {self.email}"
+       return f"{self.ism} {self.familiya}. {self.email} {self.adress} "\
 
-class Admin(Foydalanuvchi):
-    def __init__(self, ismi, familiyasi, email):
-        super().__init__(ismi, familiyasi, email)
-
-    def ban_user(self):
-        print("Foydalanuvchi bloklandi")
-
-    def get_info(self):
-        return f"Admin: {super().get_info()}"
-
-
-class Talaba:
-    def __init__(self, ismi, familiyasi):
-        self.ismi = ismi
-        self.familiyasi = familiyasi
-        self.fanlar = []
-
-
-class Fan:
-    def __init__(self, nomi):
-        self.nomi = nomi
-
-
-admin1 = Admin("Ramziddin", "Bekjonov", "ramziddinbekjonov@gmail.com")
-print(admin1.get_info())
-admin1.ban_user()
-
-
-talaba1 = Talaba("Ali", "Valiyev")
-fan1 = Fan("Matem")
-fan2 = Fan("Informatika")
+nomzot1 = User('Ismi:'"Ramziddin", "Bekjonov", 'email:'"ramziddinbekjonov@gmail.com" ,'Manzil:'"Xorazm"'da tugilgan')
+print(nomzot1.get_info())
